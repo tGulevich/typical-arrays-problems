2c7a908903eeeb23004d8a1e55ac8a841b4df088
tGulevich <t.ananich@gmail.com>
2020-02-20 10:37:12 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-
 exports.min = function min (array) {
-  return 0;
+  array = array || [];
+  return (array.length ? Math.min(...array) : 0)
 }
 
 exports.max = function max (array) {
-  return 0;
+  array = array || [];
+  return (array.length ? Math.max(...array) : 0);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  array = array || [];
+  return (array.length ? array.reduce((a,c) => a + c) / array.length : 0);
 }
+
